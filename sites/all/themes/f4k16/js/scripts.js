f1210c2b147f1024074427cc8970eb5078cbf6f2
luis mori <luismorith@gmail.com>
2016-04-13 08:47:30 -0500

    cambios

--- sites/all/themes/f4k16/js/scripts.js
+++ sites/all/themes/f4k16/js/scripts.js
@@ -1,4 +1,13 @@
 jQuery(function () {
   jQuery('option[value="All"]').text('All Categories');
-  jQuery('.catalog-sidebar h2').html('<i class="fa fa-list"></i> Categories')
+  jQuery('.catalog-sidebar h2').html('<i class="fa fa-list"></i> Categories');
+  jQuery('.node-selection-link.delete').click(function () {  jQuery(this).parent().parent().parent().parent().parent().remove() });
+
+  if (jQuery('.breadcrumb li').last().text() == '') {
+    jQuery('.breadcrumb li').last().remove();
+  }
+
+  jQuery('.block-wrapper li.active-trail a').prepend('<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>');
+
+
 });
